a1c0b6a42de6a1f101bc2d3d4204a9467437b374
Emely Garcia <garciam.emm@gmail.com>
2022-04-11 14:12:15 -0600

    Fix Hero and navbar styles

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ITab } from 'src/app/services/tabs/models/tab.model';
 import { TabService } from 'src/app/services/tabs/tab.service';
 
@@ -10,7 +10,16 @@ import { TabService } from 'src/app/services/tabs/tab.service';
 export class NavbarComponent implements OnInit {
   menuItems: ITab[] = [];
   constructor(private tabService: TabService) { }
-
+  
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+      let element = document.querySelector('.navbar-container') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-inverse');
+      } else {
+        element.classList.remove('navbar-inverse');
+      }
+    }
   ngOnInit(): void {
     this.tabService.getTabs().then((tabs) => {
       this.menuItems = tabs;
