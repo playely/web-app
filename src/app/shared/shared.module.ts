f9c8376c40a8ca362af426d665970f7959dc0c3f
Emely Garcia <garciam.emm@gmail.com>
2022-04-12 00:47:20 -0600

    Relocate shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
-import { CardCarouselComponent } from './card-carousel/card-carousel.component';
+import { HeroCarouselComponent } from './carousels/hero-carousel/hero-carousel.component';
+import { CardCarouselComponent } from './carousels//card-carousel/card-carousel.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarIconComponent } from './navbar/navbar-icon/navbar-icon.component';
-import { DescriptionCardComponent } from './card-carousel/description-card/description-card.component';
-import { InnerCardComponent } from './card-carousel/inner-card/inner-card.component';
 import { FooterComponent } from './footer/footer.component';
-import { GridCarouselComponent } from './grid-carousel/grid-carousel.component';
+import { GridCarouselComponent } from './carousels/grid-carousel/grid-carousel.component';
 import { LoadingComponent } from './loading/loading.component';
 import { RouterModule } from '@angular/router';
+import { DescriptionCardComponent } from './cards/description-card/description-card.component';
+import { InnerCardComponent } from './cards/inner-card/inner-card.component';
 
 const components = [
   HeroCarouselComponent,
