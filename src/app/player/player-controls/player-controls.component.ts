d5f229abd2387cec1ee28ee4a7b2d25e4a4249fb
Emely Garcia <garciam.emm@gmail.com>
2022-04-11 23:41:23 -0600

    Add player controls

--- src/app/player/player-controls/player-controls.component.ts
+++ src/app/player/player-controls/player-controls.component.ts
@@ -8,9 +8,37 @@ import { IContent } from 'src/app/services/content/models/carousel';
 })
 export class PlayerControlsComponent implements OnInit {
   @Input() content: IContent | undefined;
+  playerState = {
+    isPlaying: false,
+    isLoading: false,
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  play(): void {
+    this.playerState.isPlaying = true;
+  }
+
+  pause(): void {
+    this.playerState.isPlaying = false;
+  }
+
+  forward(): void {
+    return;
+    this.playerState.isLoading = true;
+    setTimeout(() => {
+      this.playerState.isLoading = false;
+    }, 2000);
+  }
+
+  rewind(): void {
+    return;
+    this.playerState.isLoading = true;
+    setTimeout(() => {
+      this.playerState.isLoading = false;
+    }, 2000);
   }
 
 }
--- src/app/player/player/player.component.ts
+++ src/app/player/player/player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from 'src/app/services/content/content.service';
+import { IContent } from 'src/app/services/content/models/carousel';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-
-  constructor() { }
+  content: IContent | undefined;
+  constructor(private contentService: ContentService) { }
 
   ngOnInit(): void {
+    this.contentService.getContent('id').then((result) => {
+      setTimeout(() => {
+        this.content = result;
+      }, 3000);
+    });
   }
 
 }
