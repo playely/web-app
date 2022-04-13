65954c033323ec6e01745d2d22331caa96bff0bc
Emely Garcia <garciam.emm@gmail.com>
2022-04-13 16:46:28 -0600

    Register form

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    data: {
+      hideFooter: true,
+      cover: true,
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
     data: {
       hideFooter: true,
       cover: true,
