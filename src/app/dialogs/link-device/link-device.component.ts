ba5b1a9e3e4a737a58c2862d23820d37c1438473
Emely Garcia <garciam.emm@gmail.com>
2022-05-27 17:48:06 -0600

    Link Device action

--- src/app/dialogs/link-device/link-device.component.ts
+++ src/app/dialogs/link-device/link-device.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-link-device',
@@ -9,13 +10,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LinkDeviceComponent implements OnInit {
 
   deviceForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<LinkDeviceComponent>) {
     this.deviceForm = this.formBuilder.group({
-      device: ['', [Validators.required]]
+      code: ['', [Validators.required]]
     });
    }
 
   ngOnInit(): void {
   }
 
+  linkDevice(): void {
+    console.log('device code: ', this.deviceForm.value);
+    this.dialogRef.close();
+  }
 }
