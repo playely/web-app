f1aa622e095e97a0fa384f1fe36a19d062e25801
Emely Garcia <garciam.emm@gmail.com>
2022-05-27 21:07:54 -0600

    Fix test

--- src/app/dialogs/link-device/link-device.component.spec.ts
+++ src/app/dialogs/link-device/link-device.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/app/shared/material.module';
 
@@ -13,8 +13,8 @@ describe('LinkDeviceComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LinkDeviceComponent ],
-      imports: [ NoopAnimationsModule, ReactiveFormsModule ]
-    })
+      imports: [ NoopAnimationsModule, ReactiveFormsModule ],
+      providers: [{ provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: {} }]    })
     .compileComponents();
   });
 
