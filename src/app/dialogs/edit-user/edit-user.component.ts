28c3a5d0a9df00d35881fdf180aad81d14ec345b
Emely Garcia <garciam.emm@gmail.com>
2022-05-27 22:10:11 -0600

    Manage error and submit

--- src/app/dialogs/edit-user/edit-user.component.ts
+++ src/app/dialogs/edit-user/edit-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-user',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-user.component.scss']
 })
 export class EditUserComponent implements OnInit {
-
-  constructor() { }
+  userForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private dialogRef: MatDialogRef<EditUserComponent>) {
+    this.userForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      phoneNumber: ['', [Validators.pattern(/[0-9]+/)]]
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  editUser(): void {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+      this.dialogRef.close();
+    }
+  }
+
+  /**
+   * Handle formGroup error
+   *
+   * @param controlName control form name
+   * @param errorName error name by validators
+   * @returns 
+   */
+   handleError (controlName: string, errorName: string): boolean {
+    return this.userForm.controls[controlName].touched && this.userForm.controls[controlName].hasError(errorName);
+  }
 }
